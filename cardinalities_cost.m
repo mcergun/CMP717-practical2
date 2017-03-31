d4736985d7d3aacffe52f6a64d423eeb99caacfe
Mert Can Ergün <mcergun@windowslive.com>
2017-04-01 00:05:01 +0300

    added cost calculation script and its first outputs

---
+++ cardinalities_cost.m
@@ -0,0 +1,50 @@
+clear all;
+clc;
+close all;
+
+load('cardinalities.mat');
+load('timediffs.mat');
+
+data_size = size(cardinalities);
+
+card_diff = zeros(data_size);
+total_timediffs = zeros(data_size);
+
+card_diff(1:data_size - 1) = diff(cardinalities);
+
+for i = 1:data_size
+    total_timediffs(i) = sum(timediffs(1:i));
+end
+
+x = 1:data_size;
+p1 = plotyy(x, cardinalities, x, timediffs);
+
+title('Cardinality vs Time Difference')
+xlabel('Iteration')
+ylabel(p1(1),'Cardinality') % left y-axis 
+ylabel(p1(2),'Time Difference') % right y-axis
+print('cardinality_cost_outputs/cardinality_cost_outputscard-timediff','-dpng');
+
+figure,
+p2 = plotyy(x, cardinalities, x, total_timediffs);
+title('Cardinality vs Total Time Spent')
+xlabel('Iteration')
+ylabel(p2(1),'Cardinality') % left y-axis 
+ylabel(p2(2),'Total Time Spent') % right y-axis
+print('cardinality_cost_outputs/card-totaltime','-dpng');
+
+figure,
+p3 = plotyy(x, card_diff, x, timediffs);
+title('Cardinality Difference vs Time Difference')
+xlabel('Iteration')
+ylabel(p3(1),'Cardinality Difference') % left y-axis 
+ylabel(p3(2),'Time Difference') % right y-axis
+print('cardinality_cost_outputs/card-diff-timediff','-dpng');
+
+figure,
+p4 = plotyy(x, card_diff, x, total_timediffs);
+title('Cardinality Difference vs Total Time Spent')
+xlabel('Iteration')
+ylabel(p4(1),'Cardinality Difference') % left y-axis 
+ylabel(p4(2),'Total Time Spent') % right y-axis
+print('cardinality_cost_outputs/card-diff-totaltime','-dpng');
