e5f639852ae38d6b2fe57502740f7cd63b9ef34e
Mert Can Ergün <mcergun@windowslive.com>
2017-04-01 08:29:18 +0300

    cardinality cost graphs are squashed together

--- cardinalities_cost.m
+++ cardinalities_cost.m
@@ -17,34 +17,68 @@ for i = 1:data_size
 end
 
 x = 1:data_size;
-p1 = plotyy(x, cardinalities, x, timediffs);
 
+%% plot graphs one by one
+% p1 = plotyy(x, cardinalities, x, timediffs);
+% title('Cardinality vs Time Difference')
+% xlabel('Iteration')
+% ylabel(p1(1),'Cardinality') % left y-axis 
+% ylabel(p1(2),'Time Difference') % right y-axis
+% print('cardinality_cost_outputs/cardinality_cost_outputscard-timediff','-dpng');
+% 
+% figure,
+% p2 = plotyy(x, cardinalities, x, total_timediffs);
+% title('Cardinality vs Total Time Spent')
+% xlabel('Iteration')
+% ylabel(p2(1),'Cardinality') % left y-axis 
+% ylabel(p2(2),'Total Time Spent') % right y-axis
+% print('cardinality_cost_outputs/card-totaltime','-dpng');
+% 
+% figure,
+% p3 = plotyy(x, card_diff, x, timediffs);
+% title('Cardinality Difference vs Time Difference')
+% xlabel('Iteration')
+% ylabel(p3(1),'Cardinality Difference') % left y-axis 
+% ylabel(p3(2),'Time Difference') % right y-axis
+% print('cardinality_cost_outputs/card-diff-timediff','-dpng');
+% 
+% figure,
+% p4 = plotyy(x, card_diff, x, total_timediffs);
+% title('Cardinality Difference vs Total Time Spent')
+% xlabel('Iteration')
+% ylabel(p4(1),'Cardinality Difference') % left y-axis 
+% ylabel(p4(2),'Total Time Spent') % right y-axis
+% print('cardinality_cost_outputs/card-diff-totaltime','-dpng');
+
+%% plot graphs in one window 
+figure,
+subplot(2, 2, 1);
+p1 = plotyy(x, cardinalities, x, timediffs);
 title('Cardinality vs Time Difference')
 xlabel('Iteration')
 ylabel(p1(1),'Cardinality') % left y-axis 
 ylabel(p1(2),'Time Difference') % right y-axis
-print('cardinality_cost_outputs/cardinality_cost_outputscard-timediff','-dpng');
 
-figure,
+subplot(2, 2, 2)
 p2 = plotyy(x, cardinalities, x, total_timediffs);
 title('Cardinality vs Total Time Spent')
 xlabel('Iteration')
 ylabel(p2(1),'Cardinality') % left y-axis 
 ylabel(p2(2),'Total Time Spent') % right y-axis
-print('cardinality_cost_outputs/card-totaltime','-dpng');
 
-figure,
+subplot(2, 2, 3)
 p3 = plotyy(x, card_diff, x, timediffs);
 title('Cardinality Difference vs Time Difference')
 xlabel('Iteration')
 ylabel(p3(1),'Cardinality Difference') % left y-axis 
 ylabel(p3(2),'Time Difference') % right y-axis
-print('cardinality_cost_outputs/card-diff-timediff','-dpng');
 
-figure,
+subplot(2, 2, 4)
 p4 = plotyy(x, card_diff, x, total_timediffs);
 title('Cardinality Difference vs Total Time Spent')
 xlabel('Iteration')
 ylabel(p4(1),'Cardinality Difference') % left y-axis 
 ylabel(p4(2),'Total Time Spent') % right y-axis
-print('cardinality_cost_outputs/card-diff-totaltime','-dpng');
+
+print('cardinality_cost_outputs/total-graph','-dpng');
+
