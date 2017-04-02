94acce84b6bc70f426f1d9bd390c51a4852c8912
Mert Can Ergün <mcergun@windowslive.com>
2017-04-02 06:15:00 +0300

    main script now changes various parameters automatically

--- main.m
+++ main.m
@@ -16,41 +16,43 @@ param.method = 'KSVD';
 % param.method = 'MOD';
 
 
-% parameter is changed to 8 because it is a sweet spot for 
+% parameter is changed to 8 because it is a sweet spot for
 % calculation time / performance gain
 param.nIterations = 8;
 param.useLessAtoms = [0.1 0.1 0.2 0.2 0.4 0.4 0.7 0.7 1 1 1 1 1 1 1];
 
-param.initType = 'RandomPatches'; 
-param.initType = 'DCT'; 
-%  param.initType = 'Input'; 
+param.initType = 'RandomPatches';
+param.initType = 'DCT';
+%  param.initType = 'Input';
 
 %% Test is run here
 results_PSNR = zeros(image_count, 4);
 size(results_PSNR)
 results_img = cell(1, image_count);
 
-for patch_size = 4:4:16
-    for sigma = 5:5:80
-        param.noiseSig = sigma;
-        param.nAtoms = 100;
-        param.patchSize = [patch_size patch_size];
-        param.groundTruthData.patchSize = param.patchSize;
-        
-        for i=1:image_count
-            close all;
-            img = imread(filenames{i});
-            [local_PSNRs, local_imgs] = Image_Denoising_Test_Custom(img, param);
-            results_img(i) = {local_imgs};
-            results_PSNR(i, :) = local_PSNRs;
+for patch_size = 4:4:12
+    for sigma = 5:5:60
+        for atoms = 8:12
+            param.noiseSig = sigma;
+            param.nAtoms = atoms^2;
+            param.patchSize = [patch_size patch_size];
+            param.groundTruthData.patchSize = param.patchSize;
+            
+            for i=1:image_count
+                close all;
+                img = imread(filenames{i});
+                [local_PSNRs, local_imgs] = Image_Denoising_Test_Custom(img, param);
+                results_img(i) = {local_imgs};
+                results_PSNR(i, :) = local_PSNRs;
+            end
+            
+            folder_path = sprintf('results/sigma%datoms%dsize%d/', param.noiseSig, ...
+                param.nAtoms, param.patchSize(1));
+            
+            mkdir(folder_path);
+            
+            save(strcat(folder_path, 'img.mat'), 'results_img');
+            save(strcat(folder_path, 'PSNR.mat'), 'results_PSNR');
         end
-        
-        folder_path = sprintf('results/sigma%datoms%dsize%d/', param.noiseSig, ...
-            param.nAtoms, param.patchSize(1));
-
-        mkdir(folder_path);
-
-        save(strcat(folder_path, 'img.mat'), 'results_img');
-        save(strcat(folder_path, 'PSNR.mat'), 'results_PSNR');
     end
 end
