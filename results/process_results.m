b91ef05cd291abdc1c80b39f1c5a2cab41e8e81a
Mert Can Ergün <mcergun@windowslive.com>
2017-04-02 12:35:26 +0300

    result processor can plot nice PSNR graphs now

--- results/process_results.m
+++ results/process_results.m
@@ -4,18 +4,52 @@ clc
 
 cd('.');
 
-folder_path = 'sigma5atoms144size8';
+mkdir('output_plots');
 
-[results_PSNR, results_img] = gather_results(folder_path);
+results_PSNR = zeros(12, 3, 4);
+results_img = cell(12, 3);
 
-image_count = size(results_PSNR, 1);
+arr = 5:5:60;
 
-for i=1:image_count
-    imageset = uint8(results_img{i});
-    figure
-    for j=1:4
-        subplot(2, 2, j)
-        imshow(imageset(:, :, j))
-        title(sprintf('PSNR %f', results_PSNR(i, j)));
-    end
+for sigma=arr
+    folder_path = sprintf('sigma%datoms100size8', sigma);
+    fprintf('Reading folder %s\n', folder_path);
+    [results_PSNR(sigma/5, :, :), results_img(sigma/5, :)] = ...
+        gather_results(folder_path);
 end
+
+
+data_count = size(results_PSNR, 1);
+image_count = size(results_PSNR, 2);
+
+noisy_psnr = zeros(image_count, data_count);
+dct_wo_ovr_psnr = zeros(image_count, data_count);
+dct_w_ovr_psnr = zeros(image_count, data_count);
+ksvd_psnr = zeros(image_count, data_count);
+
+noisy_psnr(1:3, :) = results_PSNR(:, 1:3, 1)';
+dct_wo_ovr_psnr(1:3, :) = results_PSNR(:, 1:3, 2)';
+dct_w_ovr_psnr(1:3, :) = results_PSNR(:, 1:3, 3)';
+ksvd_psnr(1:3, :) = results_PSNR(:, 1:3, 4)';
+
+xscale = 5:5:60;
+
+plot(xscale, ksvd_psnr' - dct_w_ovr_psnr');
+hold on
+plot(xscale, zeros(1, 12), ':');
+legend('barbara.png','foreman.tif', 'peppers256.png', 'zero gain line');
+ylabel('K-SVD Gain over DCT with Overlap Method');
+xlabel('Sigma Values');
+print('-dpng', fullfile('output_plots', 'filename.png'));
+
+% for i=1:data_count
+%     for j=1:image_count
+%         imageset = uint8(results_img{i, j});
+%         figure
+%         for k=1:4
+%             subplot(2, 2, k)
+%             imshow(imageset(:, :, k))
+%             title(sprintf('PSNR %f', results_PSNR(i, j, k)));
+%         end
+%     end
+% end
